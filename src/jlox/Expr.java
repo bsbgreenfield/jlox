00c9c5a0ffdecee68f7f212716ac3a388066212e
benjamingreenfield <bsbgreenfield@gmail.com>
2024-01-01 18:30:18 -0800

    statements

--- src/jlox/Expr.java
+++ src/jlox/Expr.java
@@ -3,16 +3,12 @@ package jlox;
 import java.util.List;
 
 abstract class Expr {
-  interface Visitor<R> {
+   interface Visitor<R> {
     R visitBinaryExpr(Binary expr);
-
     R visitGroupingExpr(Grouping expr);
-
     R visitLiteralExpr(Literal expr);
-
     R visitUnaryExpr(Unary expr);
   }
-
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
@@ -29,7 +25,6 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
-
   static class Grouping extends Expr {
     Grouping(Expr expression) {
       this.expression = expression;
@@ -42,7 +37,6 @@ abstract class Expr {
 
     final Expr expression;
   }
-
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
@@ -55,7 +49,6 @@ abstract class Expr {
 
     final Object value;
   }
-
   static class Unary extends Expr {
     Unary(Token operator, Expr right) {
       this.operator = operator;
@@ -64,14 +57,11 @@ abstract class Expr {
 
     @Override
     <R> R accept(Visitor<R> visitor) {
-
       return visitor.visitUnaryExpr(this);
-
     }
 
     final Token operator;
     final Expr right;
-    
   }
 
   abstract <R> R accept(Visitor<R> visitor);
