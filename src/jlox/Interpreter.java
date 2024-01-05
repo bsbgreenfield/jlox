6075bf2f3583bbdc1566a629247cbe413f2b1ff4
benjamingreenfield <bsbgreenfield@gmail.com>
2024-01-04 23:14:29 -0800

    more statements

--- src/jlox/Interpreter.java
+++ src/jlox/Interpreter.java
@@ -28,6 +28,25 @@ public class Interpreter implements Expr.Visitor<Object>,
     public Object visitLiteralExpr(Expr.Literal expr){
         return expr.value;
     }
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr){
+        /**
+         * by returning the actual Tokem, we can either 
+         * return boolean or the truthy or falsey object
+         * being evalueted
+         * ex: return "hi" or false returns "hi"
+         * as opposed to true
+         */
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+          if (isTruthy(left)) return left;
+        } else {
+          if (!isTruthy(left)) return left;
+        }
+    
+        return evaluate(expr.right);
+    }
 
     @Override
     public Object visitUnaryExpr(Expr.Unary expr){
@@ -129,7 +148,17 @@ public class Interpreter implements Expr.Visitor<Object>,
         evaluate(stmt.expression);
         return null;
     }
-
+    
+    @Override
+    public Void visitIfStmt(Stmt.If stmt){
+        if (isTruthy(evaluate(stmt.condition))){
+            execute(stmt.thenBranch);
+        }
+        else if (stmt.elseBranch != null){
+            execute(stmt.thenBranch);
+        }
+        return null;
+    }
     @Override
     public Void visitPrintStmt(Stmt.Print stmt){
         Object value = evaluate(stmt.expression);
@@ -146,6 +175,14 @@ public class Interpreter implements Expr.Visitor<Object>,
       
           environment.define(stmt.name.lexeme, value);
           return null;
+    }
+
+    @Override
+    public Void visitWhileStmt(Stmt.While stmt){
+        while(isTruthy(evaluate(stmt.condition))){
+            execute(stmt.body);
+        }
+        return null;
     }
 
     @Override
